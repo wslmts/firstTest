f915507b8f5ac17e3ad193d37742eb0cffc191c1
cuicui.zhu <cuicui.zhu@dajie-inc.com>
2016-03-02 15:56:41 +0800

    ie placehoder

---
+++ ie_placeholder/iepl.js
@@ -0,0 +1,30 @@
+var IEPlaceholder={
+    init:function(){
+     // if(!'placeholder' in document.createElement('input')){
+         $('input:text[placeholder]').each(function(i,v){
+            var txt=$(this).attr('placeholder');
+            var pos = $(this).position();
+             var w=$(this).width();
+            $('<span class="ie_place">'+txt+'</span>').css({
+                left:pos.left,top:pos.top+2
+            }).insertAfter($(this));
+         });
+        this._bindEvent();
+     // }
+    },
+    _bindEvent:function(){
+        $(".ie_place").bind("click",function(){
+            $(this).hide();
+            $(this).prev('input').focus();
+        });
+        var input=$('input:text[placeholder]');
+        input.bind("blur",function(){
+            if($.trim($(this).val())==""){
+                $(".ie_place").show();
+            }
+        });
+        input.bind("focus",function(){
+            $(this).next(".ie_place").hide();
+        });
+    }
+}
