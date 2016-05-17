bd463581de2fe24016ba46c1d7b57737bfd48c78
cuicui.zhu <cuicui.zhu@dajie-inc.com>
2016-05-17 10:32:06 +0800

    app not show layer

--- worker/1.js
+++ worker/1.js
@@ -2,7 +2,7 @@ var temp=0;
 self.onmessage = function(event) {
 //接收worker1传递过来的值,存储在event.data中
 var n = parseInt(event.data);
-temp+=n*Math.random()*n*100000000000000000000;
+temp+=n*Math.random()*n*1000000000000;
 self.postMessage("第"+event.data+"条运算完毕，结果为："+temp);
 //向网页文件回传数据
 }
