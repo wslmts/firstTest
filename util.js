e5b7fa94a78e4955a06f61fd07ff52302aebab94
cuicui.zhu <cuicui.zhu@dajie-inc.com>
2017-01-10 14:38:21 +0800

    more

--- util.js
+++ util.js
@@ -57,5 +57,15 @@ var util={
       if(text.length<=1) return true;
         if(text.charAt(0)!=text.charAt(text.length-1)) return false;
         return this.isPalindrome(text.substr(1,text.length-2))
+    },
+    uniqueArray:function(a){
+     var b=[];var obj={};
+      for(var i=0;i< a.length;i++){
+         if(!obj[a[i]]){
+             obj[a[i]]=true;
+             b.push(a[i])
+         }
+      }
+        return b;
     }
 }
